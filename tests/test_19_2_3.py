feb3c96803147d7fe4536d7936f985b8cc97df94
DariaSem <sdasha9328@gmail.com>
2020-12-18 02:11:41 +0300

    Added calculator tests

--- tests/test_19_2_3.py
+++ tests/test_19_2_3.py
@@ -9,11 +9,23 @@ class TestCalc:
     def test_multiply_calculate_correctly(self):
         assert self.calc.multiply(self, 2, 2) == 4
 
+    def test_multiply_calculate_failed(self):
+        assert self.calc.multiply(self, 2, 2) == 5
+
     def test_division_calculate_correctly(self):
         assert self.calc.division(self, 2, 2) == 1
+
+    def test_division_calculate_failed(self):
+        assert self.calc.division(self, 2, 2) == 0
 
     def test_subtraction_calculate_correctly(self):
         assert self.calc.subtraction(self, 2, 2) == 0
 
+    def test_subtraction_calculate_failed(self):
+        assert self.calc.subtraction(self, 2, 2) == 2
+
     def test_adding_calculate_correctly(self):
         assert self.calc.adding(self, 2, 2) == 4
+
+    def test_adding_calculate_failed(self):
+        assert self.calc.adding(self, 2, 2) == 3
